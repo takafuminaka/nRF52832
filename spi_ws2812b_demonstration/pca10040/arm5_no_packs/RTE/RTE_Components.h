65ad33daa76788fdf0da27104dae7830ba33aba6
Takafumi Naka <naka.takafumi@gmail.com>
2016-02-11 22:25:37 +0900

    small modify

--- spi_ws2812b_demonstration/pca10040/arm5_no_packs/RTE/RTE_Components.h
+++ spi_ws2812b_demonstration/pca10040/arm5_no_packs/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'spi_master_pca10040' 
+ * Project: 'spi_ws2812b_pca10040' 
  * Target:  'nrf52832_xxaa' 
  */
 
