e05c5664f4400824b510bf8ee5629b5c526a26db
Takafumi Naka <naka.takafumi@gmail.com>
2016-02-21 10:00:43 +0900

    Modified Comments

--- i2s_ws2812b_demostration_planB/project.h
+++ i2s_ws2812b_demostration_planB/project.h
@@ -5,11 +5,11 @@
  * the file.
  *
  */
-#define NUM_LEDS					(240)		//	Count of LEDs
-#define MAX_INTENSE					(64)		//
-#define MAX_INTENSE2				(255)
-#define MAX_INTENSE3				(64)
-#define MIN_INTENSE					(1)
-#define	ROW_SIZE					(17)			// Count of LEDs for each line
-#define CURRENT_LIMIT				(1000) 			// Current limit of LEDs (mA)
-#define	FADE_IN_MS			(6000)
+#define NUM_LEDS			(240)		// Number of LEDs (LED総数）
+#define MAX_INTENSE		(64)		// Max intense of random LEDs for "flashing_random" (flashing_random用のランダム色LEDの最大輝度)
+#define MAX_INTENSE2	(255)		// Max intense of "shooting start" running bright LEDs for all demos.
+#define MAX_INTENSE3	(64)		// Max intense of "rainbow LEDs" for "running_rainbow" and "running_rainbowv" demos. 
+#define MIN_INTENSE		(1)			// Minimum intense of randaom LEDs for "flashing_random" (flashing_random用のランダム色LEDの最小輝度)
+#define	ROW_SIZE			(17)		// Count of LEDs for each line (1巻き分のLEDの数)
+#define CURRENT_LIMIT	(1500) 	// Current limit of LEDs (mA)	(総電流制限値)
+#define	FADE_IN_MS		(6000)	// Fade in/out period (フェードイン・アウトの秒数）
